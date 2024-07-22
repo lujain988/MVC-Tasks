76a30a81de3066e9291ca09d4c074a6596a417d0
lujain988 <lujainalazzam98@gmail.com>
2024-07-22 17:42:12 +0300

    Task 1

---
+++ Task1 7-22-2024/Task1 7-22-2024/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1_7_22_2024.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Venues()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        // Action for VenueDetails
+        public ActionResult VenueDetails(int id)
+        {
+            // Logic to get venue details by id
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+      
+
+      
+
+
+
+    }
+}
---
+++ Task1 7-22-2024/Task1 7-22-2024/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task1_7_22_2024.MvcApplication" Language="C#" %>
